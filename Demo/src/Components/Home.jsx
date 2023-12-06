d9b36f31e0070cd53ef2597a70b8a9df4d9ccfea
Sumedhk18 <sumedhkamalapurkar@opticalarc.com>
2023-12-06 10:21:00 +0530

    First commit

---
+++ Demo/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+
+    <div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem ad cumque sunt? Aspernatur quidem provident nesciunt, eius tempore ipsam libero.</p>
+        <Navbar/>
+    </div>
+    
+  )
+}
+
+export default Home
